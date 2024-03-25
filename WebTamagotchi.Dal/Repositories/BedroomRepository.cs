4425a432eb3ff45dac7492fadfaede8222cec8dd
Yevhenii Saveliev <88836913+saveliyevyevhenii@users.noreply.github.com>
2024-03-25 22:00:30 +0200

    bedroom repository

---
+++ WebTamagotchi.Dal/Repositories/BedroomRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebTamagotchi.Dal.Repositories.Interfaces;
+using WebTamagotchi.GameLogic.Models;
+
+namespace WebTamagotchi.Dal.Repositories;
+
+public class BedroomRepository(WebTamagotchiDbContext dbContext) : IBedroomRepository
+{
+    public async Task<IEnumerable<Bedroom>> GetAll(CancellationToken cancellationToken)
+    {
+        return await dbContext.Bedrooms.ToListAsync(cancellationToken);
+    }
+
+    public async Task<Bedroom?> Get(string name, CancellationToken cancellationToken)
+    {
+        return await dbContext.Bedrooms.SingleOrDefaultAsync(x => x.Name == name, cancellationToken);
+    }
+
+    public async Task Create(Bedroom bedroom, CancellationToken cancellationToken)
+    {
+        dbContext.Bedrooms.Add(bedroom);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task Update(Bedroom bedroom, CancellationToken cancellationToken)
+    {
+        dbContext.Bedrooms.Update(bedroom);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task Delete(Bedroom bedroom, CancellationToken cancellationToken)
+    {
+        dbContext.Bedrooms.Remove(bedroom);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ WebTamagotchi.Dal/Repositories/Interfaces/IBedroomRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebTamagotchi.GameLogic.Models;
+
+namespace WebTamagotchi.Dal.Repositories.Interfaces;
+
+public interface IBedroomRepository
+{
+    Task<IEnumerable<Bedroom>> GetAll(CancellationToken cancellationToken);
+
+    Task<Bedroom?> Get(string name, CancellationToken cancellationToken);
+
+    Task Create(Bedroom bedroom, CancellationToken cancellationToken);
+
+    Task Update(Bedroom bedroom, CancellationToken cancellationToken);
+    
+    Task Delete(Bedroom bedroom, CancellationToken cancellationToken);
+}
